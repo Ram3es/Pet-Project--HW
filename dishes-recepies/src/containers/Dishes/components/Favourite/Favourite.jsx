43d6f04734afa819be280a88f7e8960abe98ed9e
Roman Luzan <romaluzan1@gmail.com>
2021-12-12 19:30:50 +0200

    added favourites tab and logic

--- dishes-recepies/src/containers/Dishes/components/Favourite/Favourite.jsx
+++ dishes-recepies/src/containers/Dishes/components/Favourite/Favourite.jsx
@@ -1,5 +1,50 @@
+import { Card, CardMedia, CardContent, Typography, CardActions, Button } from "@material-ui/core";
+import { useState } from "react";
+import { useEffect } from "react";
+import "./index.scss";
 const Favourite = () => {
-  return <div>Favourite</div>;
+  
+  const [dishes, setDishes] = useState([])
+ 
+  useEffect(()=>{
+    const dishes = JSON.parse(localStorage.getItem("data")) || [];
+    setDishes(dishes)
+  },[])
+
+  const handleRemove = (id) =>{
+    const cutDishesesList = dishes.filter(item => item.idMeal !== id)
+    localStorage.setItem("data",JSON.stringify(cutDishesesList) )
+    setDishes(cutDishesesList)
+    console.log(cutDishesesList)
+    
+  }
+  
+
+  return (
+    <div className="container">
+      {dishes && dishes.map((value) => (
+        <div key={Math.random()} className="content">
+          <Card sx={{ maxWidth: 245}}>
+            <CardMedia component="img" height="250" image={value.strMealThumb} alt="green iguana" />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                {value.strMeal}
+              </Typography>
+              <Typography variant="body2" color="initial" component="div">
+                <div className="instruction">{value.strInstructions}</div>
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small"  onClick={()=> handleRemove(value.idMeal)} color="secondary">
+                {" "}
+                Remove{" "}
+              </Button>
+            </CardActions>
+          </Card>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Favourite;
--- dishes-recepies/src/containers/Dishes/components/RandomDish/RandomDish.jsx
+++ dishes-recepies/src/containers/Dishes/components/RandomDish/RandomDish.jsx
@@ -1,5 +1,59 @@
-const RandomDish = () => {
-  return <div>RandomDish</div>;
+import { fetchMeal } from "../store/service"
+import "./index.scss"
+import { Card, CardMedia, CardContent, Typography, CardActions, Button } from "@material-ui/core"
+import { useEffect } from "react";
+import { useState } from "react";
+
+const RandomDish =  () => {
+  const [value, setValue] = useState("")
+
+  const fetchRecepies = async () =>{
+    const {data} = await fetchMeal()
+    console.log(data.meals[0]);
+
+    setValue(data.meals[0])
+  }
+  useEffect(()=>{
+    fetchRecepies()
+  },[])
+
+  const handleClick = () =>{
+    fetchRecepies()
+  }
+
+  const handleAddDish = () =>{
+    const dishes = JSON.parse(localStorage.getItem("data")) || [];
+    const updateData = dishes.concat([value])
+    localStorage.setItem("data", JSON.stringify( updateData))
+
+
+  }
+  
+  
+  
+  return ( value ?
+  <div className="container" >
+    <div className="content">  <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        height="250"
+        image= {value.strMealThumb }
+        alt="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {value.strMeal}
+        </Typography>
+        <Typography variant="body2" color="secondary"  component="div">
+          <div className="instruction">{value.strInstructions }</div>
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small" onClick={handleClick} color="secondary" >Skip</Button>
+        <Button size="small" onClick={handleAddDish} color="primary">Add to Favourites</Button>
+      </CardActions>
+    </Card></div>
+  </div> : null);
 };
 
 export default RandomDish;
