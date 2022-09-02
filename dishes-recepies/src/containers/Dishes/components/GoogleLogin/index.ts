a06229e04f12cd936fa057af7789412dfc28110b
Roman Luzan <romaluzan1@gmail.com>
2022-09-02 17:34:28 +0300

    added-google-login

---
+++ dishes-recepies/src/containers/Dishes/components/GoogleLogin/index.ts
@@ -0,0 +1,2 @@
+
+export {default as GoogleLogin} from "./google-login"
--- dishes-recepies/src/containers/Dishes/components/MainContent/MainContent.jsx
+++ dishes-recepies/src/containers/Dishes/components/MainContent/MainContent.jsx
@@ -1,6 +1,12 @@
 import "./index.scss";
+import GoogleLogin from '../GoogleLogin/google-login'
+
 
 const MainContent = () => {
-  return <div className="base ">World`s Dishes</div>;
+  return (<><div className="base ">World`s Dishes</div>
+  <GoogleLogin/>
+  </>)
+  
+
 };
 export default MainContent;
